ab5169a0b5e7fe517ae16bd4e0a13ce51fa7027f
Óscar Cortés Sempere <157126187+oscarcortess16@users.noreply.github.com>
2026-02-17 10:02:29 +0100

    Create Data_Science_Skills.sql
    LINKEDIN SQL INTERVIEW QUESTION
    Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. 
    You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
    
    Write a query to list the candidates who possess all of the required skills for the job. 
    Sort the output by candidate ID in ascending order.

---
+++ queries/Data_Science_Skills.sql
@@ -0,0 +1,23 @@
+"""
+  LINKEDIN SQL INTERVIEW QUESTION
+Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. 
+You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for the job. 
+Sort the output by candidate ID in ascending order. 
+"""
+
+SELECT
+  candidate_id
+FROM (
+SELECT
+  candidate_id,
+  COUNT(skill)
+FROM candidates 
+WHERE candidate_id IN 
+                 (SELECT candidate_id
+                 FROM candidates 
+                 WHERE skill IN ('Python', 'Tableau', 'PostreSQL'))  
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+)t
