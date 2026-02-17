83d31e9e455ba0dd328e701ad12c29f878ce3835
Óscar Cortés Sempere <157126187+oscarcortess16@users.noreply.github.com>
2026-02-17 10:08:24 +0100

    Create Page_with_No_Likes.sql
    FACEBOOK SQL INTERVIEW QUESTION
    Assume you're given two tables containing data about Facebook Pages 
    and their respective likes (as in 'Like a Facebook Page').
    
    Write a query to return the IDs of the Facebook pages that have zero likes. 
    The output should be sorted in ascending order based on the page IDs.

---
+++ queries/Page_with_No_Likes.sql
@@ -0,0 +1,17 @@
+"""
+  FACEBOOK SQL INTERVIEW QUESTION
+Assume you're given two tables containing data about Facebook Pages 
+and their respective likes (as in 'Like a Facebook Page').
+
+Write a query to return the IDs of the Facebook pages that have zero likes. 
+The output should be sorted in ascending order based on the page IDs.
+  
+"""
+
+SELECT
+  p.page_id
+FROM pages p
+LEFT JOIN page_likes pl
+  ON p.page_id = pl.page_id
+WHERE liked_date IS NULL
+ORDER BY p.page_id ASC;
