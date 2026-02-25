d222a171c18181f12d7e73e37af65eef40d03c77
Óscar Cortés Sempere <157126187+oscarcortess16@users.noreply.github.com>
2026-02-25 20:04:42 +0100

    Create 12. Second_day_Confirmation.sql
    TikTok SQL Interview Question
    Assume you're given tables with information about TikTok user sign-ups and confirmations through email and text. 
    New users on TikTok sign up using their email addresses, and upon sign-up, 
    each user receives a text message confirmation to activate their account.
    
    Write a query to display the user IDs of those who did not confirm their sign-up on the first day, 
    but confirmed on the second day.
    
    Definition:
    
    'action_date' refers to the date when users activated their accounts and confirmed their sign-up through text messages.

---
+++ queries/12. Second_day_Confirmation.sql
@@ -0,0 +1,20 @@
+"""
+  TikTok SQL Interview Question
+Assume you're given tables with information about TikTok user sign-ups and confirmations through email and text. 
+New users on TikTok sign up using their email addresses, and upon sign-up, 
+each user receives a text message confirmation to activate their account.
+
+Write a query to display the user IDs of those who did not confirm their sign-up on the first day, 
+but confirmed on the second day.
+
+Definition:
+
+'action_date' refers to the date when users activated their accounts and confirmed their sign-up through text messages.
+"""
+
+SELECT
+  user_id
+FROM emails e
+LEFT JOIN texts t
+  ON e.email_id = t.email_id
+WHERE action_date = ADDDATE(signup_date, INTERVAL 1 DAY)
